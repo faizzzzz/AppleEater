69e9ef61f6ab021ee91d2bc4443163e7d5af31f9
Muhammad Murad Ghaani Siddiqui <faizsiddiqui@live.com>
2017-09-29 14:29:22 -0500

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -13,8 +13,8 @@ green = (0, 255, 0)
 
 gameDisplay = pygame.display.set_mode((xres, yres))
 
-background_image = pygame.image.load("background.jpg")
-background_image = pygame.transform.scale(background_image,(xres, yres))
+# background_image = pygame.image.load("background.jpg")
+# background_image = pygame.transform.scale(background_image,(xres, yres))
 
 snakeHead = pygame.image.load("Snake_head.png")
 snakeHead = pygame.transform.scale(snakeHead, (snakeWidth, snakeHeight))
