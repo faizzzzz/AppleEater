3b826f9a9c99db84ddb747732ffbf4856974b8f5
Muhammad Murad Ghaani Siddiqui <faizsiddiqui@live.com>
2018-01-08 19:38:24 -0600

    Update ScreenUpdate.py

--- ScreenUpdate.py
+++ ScreenUpdate.py
@@ -2,14 +2,16 @@ from Importer import *
 
 def screenUpdate():
     clock = pygame.time.Clock()
-    sb.gameDisplay.fill(sb.white)
-    sb.gameDisplay.blit(opponent_1.oppoIMG, (opponent_1.oppoXpos, opponent_1.oppoYpos))
-    sb.gameDisplay.blit(snakePlayer.snakeHead, (snakePlayer.xpos, snakePlayer.ypos))
-    pygame.draw.circle(sb.gameDisplay, sb.green, (apple.appleXpos, apple.appleYpos), 5)
-
+    sb.gameDisplay.fill(sb.white)   # sets the background color.
+    sb.gameDisplay.blit(opponent_1.oppoIMG, (opponent_1.oppoXpos, opponent_1.oppoYpos)) # shows the opponent.
+    sb.gameDisplay.blit(snakePlayer.snakeHead, (snakePlayer.xpos, snakePlayer.ypos))    # shows the player.
+    pygame.draw.circle(sb.gameDisplay, sb.green, (apple.appleXpos, apple.appleYpos), 5) # shows the apple.
+    
+    # text for the score.
     textoppoScore = "Oppo Score: " + str(opponent_1.oppoScore)
     textplayScore = "Your Score: " + str(snakePlayer.points)
     message_display(textoppoScore, sb.xres - 100, 50, 25, sb.gameDisplay)
     message_display(textplayScore, sb.xres - 100, 25, 25, sb.gameDisplay)
-    clock.tick(60)
+    
+    clock.tick(60)      # frames per second of the game.
     pygame.display.update()
