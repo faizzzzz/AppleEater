7c8906fe6119d512f0443d0e50ed7a72fa4dbdd3
Muhammad Murad Ghaani Siddiqui <faizsiddiqui@live.com>
2018-01-08 18:58:01 -0600

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,6 +1,6 @@
 import pygame
 
-winningPoints = 10
+winningPoints = 10    # The number apples that need to be collected in order to win.
 
 xres = 800
 yres = 600
@@ -16,5 +16,6 @@ gameDisplay = pygame.display.set_mode((xres, yres))
 # background_image = pygame.image.load("background.jpg")
 # background_image = pygame.transform.scale(background_image,(xres, yres))
 
+# loading the image.
 snakeHead = pygame.image.load("Snake_head.png")
 snakeHead = pygame.transform.scale(snakeHead, (snakeWidth, snakeHeight))
