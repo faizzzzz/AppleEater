bfc214e7af8bbabb3938254bcb96668d06037894
Muhammad Murad Ghaani Siddiqui <faizsiddiqui@live.com>
2018-01-08 19:16:04 -0600

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -6,54 +6,63 @@ import Basics as sb
 class snake:
     def __init__(self):
         self.angle = 0
-        self.faceDIR = (1, 0, 0, 0)     #faceUP, faceRIGHT, faceDOWN, faceLEFT
-        self.stepINCR = 5
-        self.xpos = sb.xres/2
-        self.ypos = sb.yres/2
-        self.points = 0
-        self.pointsINCR = 1
-        self.isGameEnd = False
-        self.isScore = False
+        self.faceDIR = (1, 0, 0, 0)     #faceUP, faceRIGHT, faceDOWN, faceLEFT. Clockwise rotation
+        self.stepINCR = 5       # the amount of pixels the player moves.
+        self.xpos = sb.xres/2   # starts from the middle.
+        self.ypos = sb.yres/2   
+        self.points = 0     # the points of the player.
+        self.pointsINCR = 1 #   # the increment in points after eating 1 apple.
+        self.isGameEnd = False      # to check if the game has ended or not.
+        self.isScore = False        # to check if the max score has been reached or not.
 
     def move(self):
+        # To change the direction of the player.
         if Menu.gameRun == True:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
                         if self.faceDIR[0] == 1 or self.faceDIR[2] == 1:
+                            # the player can only face right if it is facing up or down.
                             self.angle = -90
-                            self.faceDIR = (0, 1, 0, 0)
+                            self.faceDIR = (0, 1, 0, 0)     # facing right.
 
                     if event.key == pygame.K_LEFT:
                         if self.faceDIR[0] == 1 or self.faceDIR[2] == 1:
+                            # the player can only face left if it is facing up or down.
                             self.angle = 90
-                            self.faceDIR = (0, 0, 0, 1)
+                            self.faceDIR = (0, 0, 0, 1)     # facing left.
 
                     if event.key == pygame.K_UP:
                         if self.faceDIR[1] == 1 or self.faceDIR[3] == 1:
+                            # the player can only face up if it is facing right or left.
                             self.angle = 0
-                            self.faceDIR = (1, 0, 0, 0)
+                            self.faceDIR = (1, 0, 0, 0)     # facing up.
 
                     if event.key == pygame.K_DOWN:
                         if self.faceDIR[1] == 1 or self.faceDIR[3] == 1:
+                            # the player can only face down if it is facing right or left.
                             self.angle = 180
-                            self.faceDIR = (0, 0, 1, 0)
+                            self.faceDIR = (0, 0, 1, 0)     # facing down.
 
                     if event.key == pygame.K_p:
-                        Menu.isGamePaused = True
+                        Menu.isGamePaused = True    # to pause the game.
 
                     if event.key == pygame.K_RETURN and Menu.isGamePaused == True:
-                        Menu.isGamePaused = False
+                        Menu.isGamePaused = False   # to unpause the game.
 
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
-                        quit()
+                        quit()      # to quit the game.
 
         self.snakeHead = rot_img(sb.snakeHead, self.angle)
         self.snakeHead_rect = self.snakeHead.get_rect()
 
     def motion(self):
         if Menu.isGamePaused == False and Menu.gameRun == True:
+            # To go up, the y value of the player has to decrease.
+            # To go donw, the y value of the player has to increase.
+            # To go right, the x value of the player has to increase.
+            # To go left, the x value of the player has to decrease.
             self.step = self.stepINCR
             if self.faceDIR[0] == 1:
                 self.ypos -= self.step
@@ -65,18 +74,22 @@ class snake:
                 self.xpos -= self.step
 
         if Menu.isGamePaused == True and Menu.gameRun == True:
+            # if the game is paused the player step increment is set to 0, so the player does not move.
             self.step = 0
 
     def throughWall(self):
-        if self.xpos < -50:
-            self.xpos = sb.xres
-        if self.xpos > sb.xres + 50:
-            self.xpos = -50
+        # if snake reaches the wall, it comes through from the other side.
+        if self.xpos < -sb.snakeWidth:
+            # if it reaches the left wall.
+            self.xpos = sb.xres     # the x position is set to xres, so that it comes from the right wall.
+        if self.xpos > sb.xres + sb.snakeWidth:
+            # if it reaches the left wall.
+            self.xpos = -sb.snakeWidth      # the x position is set to negative of the snake width, so that it comes from the left wall.
         if self.ypos > sb.yres:
-            self.ypos = -50
-        if self.ypos < -50:
-            self.ypos = sb.yres
-
-
+            # if it reaches the bottom.
+            self.ypos = -sb.snakeHeight     # the y position is set to the negative of the snake height, so that it comes from the top.
+        if self.ypos < -sb.snakeHeight:
+            # if it reaches the top.
+            self.ypos = sb.yres     # they y positio is set to the yres, so that it comes from the bottom.
 
 snakePlayer = snake()
